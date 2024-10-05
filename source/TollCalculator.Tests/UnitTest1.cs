ab7cbe8ff9a936eac86e1d3fb1abce04292c3a76
Robert Andreasson <robban84@gmail.com>
2024-10-05 20:33:18 +0200

    Create VS solution

---
+++ source/TollCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TollFeeCalculator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            TollCalculator tollCalculator = new TollCalculator();
+
+        }
+    }
+}
